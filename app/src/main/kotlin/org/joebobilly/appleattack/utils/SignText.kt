72679347b795c81b5bd709d716e23231d0a3c87e
X-Ter <jakubgorniak06@gmail.com>
2026-07-05 04:34:51 +0200

    make SignText.Serializer an object instead of a class
    also removed nullability from reader and writer as why would you pass null to those methods anyway

--- app/src/main/kotlin/org/joebobilly/appleattack/utils/SignText.kt
+++ app/src/main/kotlin/org/joebobilly/appleattack/utils/SignText.kt
@@ -11,27 +11,24 @@ import net.minestom.server.tag.TagWritable
 class SignText(val hasGlowingText: Boolean, val color: DyeColor, val messages: List<Component>) {
     companion object {
         fun tag(key: String): Tag<SignText?> {
-            return Tag.Structure<SignText>(key, Serializer())
+            return Tag.Structure<SignText>(key, Serializer)
         }
     }
 
-    class Serializer : TagSerializer<SignText> {
-        companion object {
-            private val hasGlowingText = Tag.Boolean("has_glowing_text").defaultValue(false)
-            private val color = TagUtils.enumTag<DyeColor>("color").defaultValue(DyeColor.BLACK)
-            private val messages = TagUtils.componentListTag("messages").defaultValue(listOf())
-        }
+    object Serializer : TagSerializer<SignText> {
+        private val hasGlowingText = Tag.Boolean("has_glowing_text").defaultValue(false)
+        private val color = TagUtils.enumTag<DyeColor>("color").defaultValue(DyeColor.BLACK)
+        private val messages = TagUtils.componentListTag("messages").defaultValue(listOf())
 
-        override fun read(reader: TagReadable?): SignText? {
-            if(reader == null) return null
+        override fun read(reader: TagReadable): SignText? {
             val hasGlowingText = reader.getTag(hasGlowingText)
             val color = reader.getTag(color)
             val messages = reader.getTag(messages)
             return SignText(hasGlowingText ?: false, color ?: DyeColor.BLACK, messages ?: listOf())
         }
 
-        override fun write(writer: TagWritable?, value: SignText?) {
-            if(writer == null || value == null) return
+        override fun write(writer: TagWritable, value: SignText?) {
+            if(value == null) return
             writer.setTag(hasGlowingText, value.hasGlowingText)
             writer.setTag(color, value.color)
             writer.setTag(messages, value.messages)
